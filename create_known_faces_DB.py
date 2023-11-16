083ace86d80c7dcf82694f9b85657548a878049e
mjm02 <143107124+mjm02@users.noreply.github.com>
2023-11-16 14:35:47 -0600

    Update create_known_faces_DB.py

--- create_known_faces_DB.py
+++ create_known_faces_DB.py
@@ -1,5 +1,6 @@
 import cv2
 import csv
+import numpy as np
 from deepface import DeepFace
 
 # CSV File Names
@@ -21,9 +22,11 @@ def load_faces_db(file_name):
 def save_faces_db(file_name, faces_db):
     try:
         with open(file_name, mode='w', newline='', encoding='utf-8') as file:
-            writer = csv.DictWriter(file, fieldnames=faces_db[0].keys())
+            writer = csv.DictWriter(file, fieldnames=['image_path', 'age', 'gender', 'race', 'emotion', 'embedding'])
             writer.writeheader()
             for face in faces_db:
+                # Convert embedding to a string for CSV storage
+                face['embedding'] = str(face['embedding'])
                 writer.writerow(face)
     except Exception as e:
         print(f"Error saving data to CSV: {e}")
@@ -32,15 +35,25 @@ def save_faces_db(file_name, faces_db):
 def analyze_and_store_face_from_path(image_path):
     try:
         # Analyze the face for attributes
-        analysis = DeepFace.analyze(image_path, actions=['age', 'gender', 'race', 'emotion'])
+        result = DeepFace.analyze(image_path, actions=['age', 'gender', 'race', 'emotion'])
 
-        # Extract main attributes
+        # Check if the result is a list and take the first item
+        analysis = result[0] if isinstance(result, list) else result
+
+        # Extract embeddings
+        embedding = DeepFace.represent(image_path, enforce_detection=False)
+
+        # Check if embedding extraction was successful
+        if embedding is None or not isinstance(embedding, (list, np.ndarray)):
+            raise ValueError(f"Failed to extract embedding from {image_path}")
+
         main_attributes = {
             'image_path': image_path,
             'age': analysis['age'],
             'gender': analysis['gender'],
             'race': analysis['dominant_race'],
-            'emotion': analysis['dominant_emotion']
+            'emotion': analysis['dominant_emotion'],
+            'embedding': embedding  # Store the embedding
         }
         return main_attributes
     except Exception as e:
@@ -60,5 +73,5 @@ def initialize_known_faces(faces_paths):
     save_faces_db(KNOWN_FACES_CSV, known_faces)
 
 # Example usage: Initialize known faces database
-faces_to_analyze = ['face1.jpg', 'face2.jpg', 'face3.jpg']  # Replace with actual image paths
+faces_to_analyze = ['Olivia1.jpg', 'Matthew1.jpg', 'William1.jpg']  # Replace with actual image paths
 initialize_known_faces(faces_to_analyze)
