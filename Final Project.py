c884fe33450b6375f786fa3c26b941cace7f47f6
wmcdowell02 <143294223+wmcdowell02@users.noreply.github.com>
2023-11-22 12:10:15 -0600

    Update Final Project.py

--- Final Project.py
+++ Final Project.py
@@ -9,4 +9,4 @@ from deepface import DeepFace
 import matplotlib.pyplot as plt
 import numpy as np
 
-DeepFace.stream(db_path='C:/Users/wmcdo/OneDrive/Fall 2023/M E 369P/Project/databases', time_threshold=5, frame_threshold=1)
+DeepFace.stream(db_path= <pathway to database folder>, time_threshold=5, frame_threshold=1)
