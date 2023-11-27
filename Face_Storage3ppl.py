06491d2ace45244d6e1c698743c1ae11296c3bd3
mjm02 <143107124+mjm02@users.noreply.github.com>
2023-11-26 20:56:53 -0600

    Add files via upload

---
+++ Face_Storage3ppl.py
@@ -0,0 +1,49 @@
+import cv2
+import os
+import numpy as np
+
+# Load a pre-trained face detection model from OpenCV
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+# Function to extract and save the best face from an image
+def extract_and_save_best_face(input_images, output_folder):
+    # Create the output folder if it doesn't exist
+    os.makedirs(output_folder, exist_ok=True)
+
+    for i, image_path in enumerate(input_images):
+        # Ensure image_path is a string
+        if not isinstance(image_path, str):
+            print(f"Error: image_path must be a string, got {type(image_path)} instead.")
+            continue
+
+        # Read the image
+        image = cv2.imread(image_path)
+        
+        # Check if the image is loaded
+        if image is None:
+            print(f"Error: Failed to load image from {image_path}")
+            continue
+
+        # Convert the image to grayscale for face detection
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        # Detect faces in the image
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+        # Find the best face based on area (largest face)
+        if len(faces) > 0:
+            best_face_idx = np.argmax([w * h for x, y, w, h in faces])
+            x, y, w, h = faces[best_face_idx]
+            best_face = image[y:y+h, x:x+w]
+            
+            # Save the best face as an image
+            output_path = os.path.join(output_folder, f"best_face_{i + 1}.jpg")
+            cv2.imwrite(output_path, best_face)
+            print(f"Best Face from Image {i + 1} saved as {output_path}")
+        else:
+            print(f"No faces detected in Image {i + 1}")
+
+# Example usage
+input_images = ['Olivia1.jpg', 'Matthew1.jpg', 'William1.jpg']  # Replace with the paths to your input images
+output_folder = 'best_faces'  # Folder where best faces will be saved
+extract_and_save_best_face(input_images, output_folder)
--- create_known_faces_DB.py
+++ create_known_faces_DB.py
@@ -1,77 +1,94 @@
 import cv2
 import csv
-import numpy as np
 from deepface import DeepFace
 
-# CSV File Names
-KNOWN_FACES_CSV = 'known_faces.csv'
+# Set the default model to VGG-Face for DeepFace
+DeepFace.build_model("VGG-Face")
 
-# Function to load faces database from a CSV file
-def load_faces_db(file_name):
-    faces_db = []
-    try:
-        with open(file_name, mode='r', newline='', encoding='utf-8') as file:
-            reader = csv.DictReader(file)
-            for row in reader:
-                faces_db.append(row)
-    except FileNotFoundError:
-        print(f"File {file_name} not found. Starting with an empty database.")
-    return faces_db
+# Load a pre-trained face detection model from OpenCV
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-# Function to save faces database to a CSV file
-def save_faces_db(file_name, faces_db):
-    try:
-        with open(file_name, mode='w', newline='', encoding='utf-8') as file:
-            writer = csv.DictWriter(file, fieldnames=['image_path', 'age', 'gender', 'race', 'emotion', 'embedding'])
-            writer.writeheader()
-            for face in faces_db:
-                # Convert embedding to a string for CSV storage
-                face['embedding'] = str(face['embedding'])
-                writer.writerow(face)
-    except Exception as e:
-        print(f"Error saving data to CSV: {e}")
+# Function to analyze face and store results
+def analyze_and_store_face(image_path):
+    # Ensure image_path is a string
+    if not isinstance(image_path, str):
+        print(f"Error: image_path must be a string, got {type(image_path)} instead.")
+        return None
 
-# Function to analyze face from an image file and store results
-def analyze_and_store_face_from_path(image_path):
+    # Read the image
+    image = cv2.imread(image_path)
+    
+    # Check if the image is loaded
+    if image is None:
+        print(f"Error: Failed to load image from {image_path}")
+        return None
+
+    # Convert the image to grayscale for face detection
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces in the image
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5, minSize=(50, 50))  # Adjust parameters
+
+    # Check if a single face is detected
+    if len(faces) != 1:
+        print(f"Error: A single face should be detected in {image_path}. Detected {len(faces)} faces.")
+        return None
+
     try:
         # Analyze the face for attributes
-        result = DeepFace.analyze(image_path, actions=['age', 'gender', 'race', 'emotion'])
+        analysis = DeepFace.analyze(image_path, actions=['age', 'gender', 'race', 'emotion'])
 
-        # Check if the result is a list and take the first item
-        analysis = result[0] if isinstance(result, list) else result
+        # Handle the analysis data as a list
+        if isinstance(analysis, list):
+            # Adjust the following line based on the actual list structure
+            analysis_data = analysis[0] if len(analysis) > 0 else {}
 
-        # Extract embeddings
-        embedding = DeepFace.represent(image_path, enforce_detection=False)
+            main_attributes = {
+                'image_path': image_path,
+                'age': analysis_data.get('age'),
+                'gender': analysis_data.get('gender'),
+                'race': analysis_data.get('dominant_race', 'N/A'),
+                'emotion': analysis_data.get('dominant_emotion', 'N/A')
+            }
+        else:
+            print(f"Unexpected data structure: {type(analysis)}")
+            return None
 
-        # Check if embedding extraction was successful
-        if embedding is None or not isinstance(embedding, (list, np.ndarray)):
-            raise ValueError(f"Failed to extract embedding from {image_path}")
-
-        main_attributes = {
-            'image_path': image_path,
-            'age': analysis['age'],
-            'gender': analysis['gender'],
-            'race': analysis['dominant_race'],
-            'emotion': analysis['dominant_emotion'],
-            'embedding': embedding  # Store the embedding
-        }
         return main_attributes
     except Exception as e:
         print(f"An error occurred during DeepFace analysis: {e}")
         return None
 
-# Function to initialize the known faces database with provided images
-def initialize_known_faces(faces_paths):
-    known_faces = []
-    for path in faces_paths:
-        face_data = analyze_and_store_face_from_path(path)
-        if face_data:
-            known_faces.append(face_data)
-        else:
-            print(f"Failed to analyze face in {path}")
+# Function to save analysis to a CSV file
+def save_analysis_to_csv(face_data, file_name='face_analysis.csv'):
+    headers = ['image_path', 'age', 'gender', 'race', 'emotion']
 
-    save_faces_db(KNOWN_FACES_CSV, known_faces)
+    try:
+        with open(file_name, 'a', newline='') as file:  # Use 'a' mode to append to the CSV
+            writer = csv.DictWriter(file, fieldnames=headers)
+            if face_data is not None:  # Check if entry is not None
+                writer.writerow(face_data)
+    except Exception as e:
+        print(f"Error saving data to CSV: {e}")
 
-# Example usage: Initialize known faces database
-faces_to_analyze = ['Olivia1.jpg', 'Matthew1.jpg', 'William1.jpg']  # Replace with actual image paths
-initialize_known_faces(faces_to_analyze)
+# Clear previous entries in the CSV file
+def clear_csv(file_name='face_analysis.csv'):
+    try:
+        with open(file_name, 'w', newline='') as file:  # Use 'w' mode to clear the CSV
+            pass
+    except Exception as e:
+        print(f"Error clearing CSV: {e}")
+
+# Paths to the best faces from the previous program
+best_face_paths = ['best_faces/best_face_1/best_face_1.jpg', 'best_faces/best_face_2/best_face_2.jpg', 'best_faces/best_face_3/best_face_3.jpg']
+
+# Clear the CSV file
+clear_csv()
+
+for path in best_face_paths:
+    face_data = analyze_and_store_face(path)
+
+    # Save the analysis results to the CSV file
+    save_analysis_to_csv(face_data)
+
+print("Face analysis completed and saved to CSV.")
