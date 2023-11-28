ec7bcfd35659f5457a1a73af895e59d389f5a6e1
wmcdowell02 <143294223+wmcdowell02@users.noreply.github.com>
2023-11-27 21:43:07 -0600

    Update create_known_faces_DB.py

--- create_known_faces_DB.py
+++ create_known_faces_DB.py
@@ -79,7 +79,7 @@ def initialize_csv(file_name):
         print(f"Error initializing CSV: {e}")
 
 # Paths to the best faces from the previous program
-best_face_paths = ['best_faces/Olivia1_face.jpg', 'best_faces/Matthew1_face.jpg', 'best_faces/William1_face.jpg']
+best_face_paths = [] # A list of strings of the paths to the best faces
 
 # Initialize the CSV file with column headers
 initialize_csv(csv_file)
